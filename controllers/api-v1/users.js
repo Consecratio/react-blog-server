85b5f4c79850b47926b7cf58845b557a3b5c23de
Consecratio <jcasique@gmail.com>
2021-10-26 17:59:34 -0700

    Auth Route middleware complete
    - Add middleware to verify a jwt
    - Import route to users.js

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const db = require('../../models')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const authLockedRoute = require('./authLockedRoute')
 
 // GET - /users -- test api endpoints
 router.get('/', (req, res) => {
